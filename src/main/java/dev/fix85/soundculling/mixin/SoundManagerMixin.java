ee0512e40cc0428a94ca18e48363a8f37673fea2
Fix85 <fix85@users.noreply.github.com>
2026-06-01 07:22:09 +0000

    SoundCulling for MC 1.21.4 (loom 1.12, cloth 17.0.144, modmenu 13.0.3; void SoundManager.play + ResourceLocation mappings)

--- src/main/java/dev/fix85/soundculling/mixin/SoundManagerMixin.java
+++ src/main/java/dev/fix85/soundculling/mixin/SoundManagerMixin.java
@@ -2,22 +2,21 @@ package dev.fix85.soundculling.mixin;
 
 import dev.fix85.soundculling.Config;
 import net.minecraft.client.resources.sounds.SoundInstance;
-import net.minecraft.client.sounds.SoundEngine;
 import net.minecraft.client.sounds.SoundManager;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundManager.class)
 public class SoundManagerMixin {
-    @Inject(method = "play(Lnet/minecraft/client/resources/sounds/SoundInstance;)Lnet/minecraft/client/sounds/SoundEngine$PlayResult;", at = @At("HEAD"), cancellable = true)
-    private void onPlay(SoundInstance sound, CallbackInfoReturnable<SoundEngine.PlayResult> cir) {
+    @Inject(method = "play(Lnet/minecraft/client/resources/sounds/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+    private void onPlay(SoundInstance sound, CallbackInfo ci) {
         if (sound != null) {
-            Identifier id = sound.getIdentifier();
+            ResourceLocation id = sound.getLocation();
             if (id != null && Config.get().isSoundBlocked(id.toString())) {
-                cir.setReturnValue(SoundEngine.PlayResult.NOT_STARTED);
+                ci.cancel();
             }
         }
     }
