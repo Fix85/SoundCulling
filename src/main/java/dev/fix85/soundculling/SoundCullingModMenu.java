c2e32e054a140140c8623ebfdd7665ed5251acaf
Fix85 <144918805+Fix85@users.noreply.github.com>
2026-06-07 01:32:59 +0500

    Optimize sound config GUI: implement virtualized search screen with preview

--- src/main/java/dev/fix85/soundculling/SoundCullingModMenu.java
+++ src/main/java/dev/fix85/soundculling/SoundCullingModMenu.java
@@ -18,53 +18,6 @@ import java.util.Map;
 public class SoundCullingModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> {
-            ConfigBuilder builder = ConfigBuilder.create()
-                    .setParentScreen(parent)
-                    .setTitle(Component.translatable("title.soundculling.config"));
-
-            ConfigCategory general = builder.getOrCreateCategory(Component.translatable("category.soundculling.general"));
-            ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-
-            // Master switch to block all sounds
-            general.addEntry(entryBuilder.startBooleanToggle(
-                    Component.translatable("option.soundculling.block_all"),
-                    Config.get().blockAll
-            )
-                    .setDefaultValue(false)
-                    .setTooltip(Component.translatable("option.soundculling.block_all.tooltip"))
-                    .setSaveConsumer(newValue -> Config.get().blockAll = newValue)
-                    .build());
-
-            general.addEntry(entryBuilder.startTextDescription(Component.literal("§7--------------------------------------§r")).build());
-
-            List<Map.Entry<ResourceKey<SoundEvent>, SoundEvent>> sounds = new ArrayList<>(
-                    BuiltInRegistries.SOUND_EVENT.entrySet()
-            );
-            sounds.sort(Comparator.comparing(entry -> entry.getKey().identifier().getPath()));
-
-            // Individual sound toggles (logically overridden when blockAll is active)
-            for (Map.Entry<ResourceKey<SoundEvent>, SoundEvent> entry : sounds) {
-                String idStr = entry.getKey().identifier().toString();
-                String path = entry.getKey().identifier().getPath();
-
-                String name = path.replace('_', ' ').replace('.', ' ');
-                if (!name.isEmpty()) {
-                    name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
-                }
-
-                general.addEntry(entryBuilder.startBooleanToggle(
-                        Component.literal(name),
-                        Config.get().soundStates.getOrDefault(idStr, false)
-                )
-                        .setDefaultValue(false)
-                        .setTooltip(Component.literal("Block sound: " + idStr))
-                        .setSaveConsumer(newValue -> Config.get().setSoundBlocked(idStr, newValue))
-                        .build());
-            }
-
-            builder.setSavingRunnable(Config::save);
-            return builder.build();
-        };
+        return SoundCullingScreen::new;
     }
 }
